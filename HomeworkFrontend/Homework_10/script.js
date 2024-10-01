77c02948c861a148de6eb4afc47c5c5c88044d13
Kateryna-Komarova <svonsonket@gmail.com>
2024-10-01 15:05:22 +0200

    Homework_10

---
+++ HomeworkFrontend/Homework_10/script.js
@@ -0,0 +1,17 @@
+console.log("Первое задание");
+
+let myNumber1 = 100 / (25 % 3);
+console.log(myNumber1);
+
+console.log("Второе задание");
+let myStringNumber = 2024;
+let myString = `Hello,  world ${myStringNumber}!`;
+console.log(myString);
+
+console.log("Третье задание");
+let isHuman = 42 == "42";
+console.log(isHuman);
+
+console.log("Четвертое задание");
+let myNumber = parseInt("123smth");
+console.log(myNumber);
